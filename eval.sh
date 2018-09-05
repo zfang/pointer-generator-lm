47454e3612529aa58f5e162223c889c801b6b7f3
Felix Fang <felixf@qualtrics.com>
2018-09-05 14:26:23 -0700

    add eval.sh

---
+++ eval.sh
@@ -0,0 +1,6 @@
+MODEL=$1
+shift
+MODE=$1
+shift
+python evaluate_model.py --rouge --decode_dir=.decode/$MODEL/$MODE --ref_dir=./ref/$MODE $@
+python evaluate_model.py --meteor --decode_dir=.decode/$MODEL/$MODE --ref_dir=./ref/$MODE  $@
