fd922245347566a0f7bcb44c5aa6090d539f8865
Felix Fang <felixf@qualtrics.com>
2018-09-20 00:27:06 -0700

    Minor change

--- eval.sh
+++ eval.sh
@@ -2,5 +2,5 @@ MODEL=$1
 shift
 MODE=$1
 shift
-python evaluate_model.py --rouge --decode_dir=.decode/$MODEL/$MODE --ref_dir=./ref/$MODE $@
-python evaluate_model.py --meteor --decode_dir=.decode/$MODEL/$MODE --ref_dir=./ref/$MODE  $@
+python evaluate_model.py --rouge --decode_dir=.decode/$MODEL/$MODE -f $@
+python evaluate_model.py --meteor --decode_dir=.decode/$MODEL/$MODE  -f $@
